57842267d2e3b1a697a15d086fae3af8b0ca75d4
Glen Pinner <glen.pinner@gmail.com>
2017-08-24 11:59:34 +0100

    Added "around" aspect example.

--- src/main/java/com/oreilly/aspects/LoggingAspect.java
+++ src/main/java/com/oreilly/aspects/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.oreilly.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,11 +15,26 @@ public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(getClass().getName());
 
-    @Before("execution(void com.oreilly..*.set*(*))")
-    public void callSetters(JoinPoint joinPoint) {
-        String method = joinPoint.getSignature().getName();
-        String arg = joinPoint.getArgs()[0].toString();
+//    @Before("execution(void com.oreilly..*.set*(*))")
+//    public void callSetters(JoinPoint joinPoint) {
+//        String method = joinPoint.getSignature().getName();
+//        String arg = joinPoint.getArgs()[0].toString();
+//
+//        logger.info("Called " + method + " with arg " + arg + " on " + joinPoint.getTarget());
+//    }
 
-        logger.info("Called " + method + " with arg " + arg + " on " + joinPoint.getTarget());
+    @Around("execution(String playGame())")
+    public Object checkForRain(ProceedingJoinPoint pjp) throws Throwable {
+        boolean rain = Math.random() < 0.5;
+        Object result = null;
+
+        if (rain) {
+            logger.info(pjp.getTarget() + " rained out");
+        } else {
+            result = pjp.proceed();
+            logger.info(result.toString());
+        }
+
+        return result;
     }
 }
